38fe8233894a2877eae6bd6ff781feffd41a4019
Julia Fehr <09jwithers@gmail.com>
2020-05-25 20:59:29 -0400

    cors not working

--- src/app.js
+++ src/app.js
@@ -6,17 +6,13 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const errorHandler = require('./error-handler')
-// const validateBearerToken = require('./validate-bearer-token')
+
 const NotefulRouter = require('./noteful/noteful-router')
 
 const app = express()
 
-// ({
-//     origin: "http://localhost:3000/"
-// })
 
 
-app.options('*',cors())
 app.use(cors({
         origin: "http://localhost:3000/"
     })
@@ -25,9 +21,9 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption))
 app.use(helmet())
 
-// app.use(validateBearerToken)
-
+app.options('*',cors())
 app.use('/api/noteful',NotefulRouter)
+
 app.use(errorHandler)
 
 module.exports = app
